af87d5c6968fdbcbfff8363fd96cbce980eb016f
jonijo06 <jonasjuergens6@gmail.com>
2022-01-26 12:29:47 +0100

    remove protected routes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import "dotenv/config.js";
 import express from "express";
 import "./db/mongoose.js";
 import users from "./routes/users.js";
-import protectedRoute from "./routes/protected.js"
 import cors from 'cors';
 
 const app = express();
@@ -11,7 +10,6 @@ const port = process.env.PORT || 5005;
 app.use(cors())
 app.use(express.json());
 app.use("/user", users);
-app.use("/info", protectedRoute);
 
 app.get("/", (req, res) => {
     res.send("<h1>Chatverse Backend</h1>");
