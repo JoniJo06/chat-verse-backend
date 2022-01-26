787d299ed611909aa2ae873a3f84b4d3147e04d6
Tobias Wutz <tobiaswutz22@gmail.com>
2022-01-26 13:09:39 +0100

    updated user Sheema

--- models/User.js
+++ models/User.js
@@ -7,8 +7,22 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true},
   first_name: { type: String, required: true},
   last_name: { type: String, required: true},
-  gender: { type: String },
-  birthday: { type: String }
+  gender: { type: String, required: true },
+  birthday: { type: String, required: true },
+  phone: { type: String, default: '' },
+  profile_pic: { type: String, default: ''},
+  public: { type: Boolean, default: false },
+  active: { type: Boolean, default: true },
+  status: { type: String, default: 'user' },
+  last_seen: { type: Number, default:Date.now() },
+  slogan: { type: String, default: 'Hallo, ich benutze Chatverse!' },
+  friends: [{ type: String }],
+  friend_requests: [{ type: String }],
+  blacklist: [{ type: String }],
+  groups: [{ type: String }],
+  chats: [{ type: String }],
+  preferences: [{ type: String }],
+
 });
 
 export default model("User", userSchema);
