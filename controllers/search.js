9ee7352dc526ae614ab8a892b20f8c1ce79a8b01
jonijo06 <jonasjuergens6@gmail.com>
2022-02-03 14:51:21 +0100

    add search function

--- controllers/search.js
+++ controllers/search.js
@@ -4,13 +4,18 @@ export const mainSearch = async (req,res) => {
   try{
 
   const {search, type} = req.query
-  const regex = new RegExp(String.raw`(.*${search}.*\w)+`)
+  const regexAll = new RegExp(String.raw`(.*${search}.*\w)+`)
     if(type === 'user'){
-    const users = await User.find({username: {$regex: regex, $options: 'gim'}})
-    res.send(users)
+    const regexSingle = new RegExp(String.raw`${search}\b`)
+
+      const users = await User.find({username: {$regex: regexAll, $options: 'gim'}, public: true, active:true}, 'username profile_pic')
+      try{
+        const user = await User.findOne({username: {$regex: regexSingle, $options: 'gi'}, public: true, active: true}, 'username profile_pic')
+        res.status(200).send({user:user,users: users})
+      }catch{
+      res.status(200).send()
+      }
     }
-      console.log(search)
-// console.log(users)
   } catch (err){
     res.status(500).json(err.message)
   }
